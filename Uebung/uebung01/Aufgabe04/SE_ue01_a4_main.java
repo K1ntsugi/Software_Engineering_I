0a96cb84108de800db5841a6cbab2f94d18e1d89
K1ntsugi <max-groetsch@gmx.de>
2021-03-26 16:22:19 +0100

    xxx

---
+++ Uebung/uebung01/Aufgabe04/SE_ue01_a4_main.java
@@ -0,0 +1,58 @@
+package Uebung.uebung01.Aufgabe04;
+
+import java.util.Scanner;
+
+public class SE_ue01_a4_main {
+
+    public static void main(String[] args) {
+
+        Scanner pangramm = new Scanner(System.in);
+        System.out.println("Prüfe ob Eingabe ein Pangramm ist: ");
+        String check = "Jeder wackere Bayer vertilgt bequem zwo Pfund Kalbshaxen.";//pangramm.nextLine();
+
+        System.out.println(check);
+
+        if (isDirtyPangramm(check)) {
+            System.out.println("Der Satz ist ein Pangramm");
+            return;
+        }
+
+        System.out.println("Der Satz ist kein Pangramm");
+
+    }
+
+    public static boolean isPangramSentence(String txt) {
+
+        if (txt.length() < 28 || !txt.contains(" ")) {
+            return false;
+        } else if (!Character.isUpperCase(txt.charAt(0))) {
+            return false;
+        } else if (!(txt.charAt(txt.length() - 1) == '?') && !(txt.charAt(txt.length() - 1) == '!')
+                && !(txt.charAt(txt.length() - 1) == '.')) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean isDirtyPangramm(String txt) {
+        if (!isPangramSentence(txt)) {
+            return false;
+        }
+
+        String alphabetString = new String("abcdefghijklmnopqrstuvwxyz");
+
+        txt = txt.toLowerCase();
+
+        for (int i = 0; i < alphabetString.length(); i++)
+        {
+            if (!txt.contains(Character.toString(alphabetString.charAt(i))))
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+}
