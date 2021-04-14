7cad874e81f4481d2811f1e8a3972b17aad6bdfb
K1ntsugi <max-groetsch@gmx.de>
2021-04-14 14:31:52 +0200

    ue2

---
+++ Uebung/uebung02/SE_ue02_a4.java
@@ -0,0 +1,23 @@
+package Uebung.uebung02;
+
+import java.util.Scanner;
+
+public class SE_ue02_a4 {
+   
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        String data;
+
+        System.out.println("Bitte Zahl einlesen: ");
+        data = read.nextLine();
+
+        System.out.println(isFloat(data));
+    }
+
+    public static boolean isFloat(String txt)
+    {
+        
+    }
+
+}
---
+++ Uebung/uebung02/a4_design.md
@@ -0,0 +1,11 @@
+# Spezifikationen
+
+## Gleitkommadarstellung
+E + 4 = 10 ^ 4
+E - 5 = 10 ^ -5
+
+5.5E+6
+
+## Funktion
+-> Ausgabe als Boolean (nichts auf die CMD)
+
